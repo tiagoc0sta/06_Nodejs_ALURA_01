02daf8129f6ea11cf8605910684797ca7e397cc7
Tiago Costa <tiagodpv@gmail.com>
2023-05-09 20:22:13 -0400

    add put

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-const http = require('http');
-const port = 3000;
+// Versao sem express
+//const http = require('http');
+// const rotas = {
+//   '/': 'Curso de Node',
+//   '/livros': 'Entrei na pag de livros',
+//   '/autores': 'Listagem de autores',
+//   '/editora': 'Pag de editora',
+//   '/sobre': 'Info sobre o projeto',
+// };
 
-const rotas = {
-  '/': 'Curso de Node',
-  '/livros': 'Entrei na pag de livros',
-  '/autores': 'Listagem de autores',
-  '/editora': 'Pag de editora',
-  '/sobre': 'Info sobre o projeto',
-};
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { 'Content-type': 'text/plain' });
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-type': 'text/plain' });
+//   res.end(rotas[req.url]);
+// });
 
-  res.end(rotas[req.url]);
-});
+// server.listen(port, () => {
+//   console.log(`Servidor escutando em http://localhost:${port}`);
+// });
 
-server.listen(port, () => {
+import app from './src/app.js';
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
   console.log(`Servidor escutando em http://localhost:${port}`);
 });
