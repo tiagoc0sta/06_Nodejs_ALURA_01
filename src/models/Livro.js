a2c05560d80facf2759b25b38de493f613cf94ce
Tiago Costa <tiagodpv@gmail.com>
2023-05-11 15:33:20 -0400

    add livros

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -5,7 +5,9 @@ const livroSchema = new mongoose.Schema({
   titulo: { type: String, required: true },
   autor: { type: String, required: true },
   editora: { type: String, required: true },
-  numeroPaginas: { type: number },
+  numeroPaginas: { type: Number },
 });
 
-const livros = mongoose.model('livros');
+const livros = mongoose.model('livros', livroSchema);
+
+export default livros;
